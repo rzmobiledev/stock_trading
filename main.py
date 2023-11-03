cd7cb83c4a41ee56475a3aa72b2184d86e5c759e
Safrizal <rzmobiledev@gmail.com>
2023-11-03 09:59:25 +0700

    first upload

---
+++ main.py
@@ -0,0 +1,27 @@
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+# STEP 1: Use http://www.alphavantage.co/documentation/#daily
+# when stock price increases/decreases by 5% between yesterday and the day before yesterday then
+
+# TODO 1. - Get yesterday's closing stock price
+
+# TODO 2. - Get the day before yesterday's closing stock price
+
+# TODO 3. - Find the positive difference between 1 and 2. eg. 40 - 20 = -20, but the possitive is 20. hints https://www.w3school.com
+
+# TODO 4. - Work out the value of 5% of yesterday's closing price
+
+# TODO 5. - if TODO 4 is True then print("Get News")
+
+# TODO 6. - Use python slice operator to create a list than contains the first 3 articles.
+
+# TODO 7. - Create a new list of the first 3 article's headline and description using list comprehension
+
+# TODO 8. - Send each article as a separate message via twilio
+
+
+#
